8ca15e4c7cf7ad9248ca434ab7c201510678b243
lucasgab <lucasgab2010@gmail.com>
2019-05-16 20:03:32 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="frente">
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'movimiento solidario independiente'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.frente{
+  background: rgb(167, 93, 93);
+  margin-top: 0px;
+  padding: 0px%;
+}
+
+</style>
